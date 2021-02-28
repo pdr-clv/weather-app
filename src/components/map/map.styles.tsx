e80fbf99620de3cfac8992579736e9140c72a9f8
pdr-clv <pdr.clv@gmail.com>
2021-02-28 22:43:03 +0300

    solving problem of ReactMapBox workerClass

--- src/components/map/map.styles.tsx
+++ src/components/map/map.styles.tsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 import ReactMapGL, { Marker } from 'react-map-gl';
-//we import this mapbox-gl.css to avoid warnings of styles for the map.
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 import pin from '../../imgs/pin.png';
+//we import this mapbox-gl.css to avoid warnings of styles for the map.
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 export const MapContainer = styled(ReactMapGL)`
   border-radius: 10px;
